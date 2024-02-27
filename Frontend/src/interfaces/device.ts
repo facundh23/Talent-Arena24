7d8162719ea434850cb6ee2b841cdcb6c5a40fc0
Carlos Blanco Benitez <carlos.blanco@external.wolterskluwer.com>
2024-02-27 18:34:23 +0100

    Map

---
+++ Frontend/src/interfaces/device.ts
@@ -0,0 +1,8 @@
+export interface Device {
+  id: string;
+  type: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+  status: string;
+}
--- Frontend/src/presentation/pages/home/HomePage.tsx
+++ Frontend/src/presentation/pages/home/HomePage.tsx
@@ -1,41 +1,55 @@
-import { useState } from "react"
-import ConsultDeviceText from "../../components/ConsultDeviceText"
-import { searchQueryUseCase } from "../../../core/use-case/search-query.use-case";
-import { CounterDevices } from "../../components/CounterDevices";
+import { useEffect, useState } from 'react';
+import ConsultDeviceText from '../../components/ConsultDeviceText';
+import { searchQueryUseCase } from '../../../core/use-case/search-query.use-case';
+import { CounterDevices } from '../../components/CounterDevices';
+import Map from '../../components/Map';
+import { Device } from '../../../interfaces/device';
+import { getAllDevices } from '../../../core/use-case/get-all-devices';
 
-const devices = [
-  {id:'dron', text:'Dron'},
-  {id:'plane', text:'Plane'},
-  {id:'ship', text:'Ship'},
-]
+const deviceTypes = [
+  { id: 'dron', text: 'Dron' },
+  { id: 'plane', text: 'Plane' },
+  { id: 'ship', text: 'Ship' },
+];
 
 interface Message {
-  text:string;
+  text: string;
 }
 
 export const HomePage = () => {
-
+  const [devices, setDevices] = useState<Device[]>([]);
   const [isLoading, setIsloading] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
 
+  useEffect(() => {
+    getAllDevices().then(setDevices)
+  }, [])
 
-  const handlePost = async(text:string, selectedOption:string, id:string) => {
+
+  const handlePost = async (
+    text: string,
+    selectedOption: string,
+    id: string
+  ) => {
     setIsloading(true);
-    const newQuery = `${text} - ${selectedOption}`
-    setMessages((prev) => [...prev, {text:newQuery}]);
+    const newQuery = `${text} - ${selectedOption}`;
+    setMessages((prev) => [...prev, { text: newQuery }]);
 
-    const data = await searchQueryUseCase(text, selectedOption, id)
-    if(!data.ok) return;
+    const data = await searchQueryUseCase(text, selectedOption, id);
+    if (!data.ok) return;
 
     setIsloading(false);
-  }
-  
+  };
+
   return (
     <div>
-      <ConsultDeviceText placeholder="Create your device" onSendQuery={handlePost} options={devices} />
+      <ConsultDeviceText
+        placeholder="Create your device"
+        onSendQuery={handlePost}
+        options={deviceTypes}
+      />
       <CounterDevices />
+      <Map devices={devices}></Map>
     </div>
-  )
-}
-
-
+  );
+};
