7abfe10e5abcc4f32c031f5cd13ea6e1b2e2c254
Facundo Herrera <facundhfed@gmail.com>
2024-02-27 12:59:10 +0100

    Folder structure

---
+++ Frontend/src/TalentFE.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TalentFE = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import {TalentFE} from './TalentFE.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TalentFE />
   </React.StrictMode>,
 )
