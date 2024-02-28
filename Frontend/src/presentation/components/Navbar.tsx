10e1699ac6616f3b4cbc0ecad11db83a75dfdbec
Facundo Herrera <facundhfed@gmail.com>
2024-02-28 12:05:00 +0100

    Logo completed

--- Frontend/src/presentation/components/Navbar.tsx
+++ Frontend/src/presentation/components/Navbar.tsx
@@ -1,11 +1,11 @@
-
+import logoImg from '../../../src/assets/img/logoImg.jpg'
 interface Props {
     isOpen:() => void
 }
 const Navbar = ({isOpen}:Props) => {
     return (
         <nav className="navbarColor border-4 border-white p-4 flex items-center px-2 py-2 justify-between mb-4 w-90% rounded-lg">
-            <div>Logo</div>
+            <img src={logoImg} className='w-14 h-10 rounded-lg'/>
             <button onClick={isOpen} className='bg-white text-black p-2 rounded-lg font-bold cursor-pointer navbarBoxColor hover:text-white transition-all duration-700 ease-in-out'>Add Device</button>
         </nav>
     )
