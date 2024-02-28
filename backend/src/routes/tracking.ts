bfe21d11c8d2885b00199751d160d0db823c26a9
Carlos Blanco Benitez <carlos.blanco@external.wolterskluwer.com>
2024-02-28 10:51:06 +0100

    Add creation

--- backend/src/routes/tracking.ts
+++ backend/src/routes/tracking.ts
@@ -3,6 +3,15 @@ import { TrackingHandler } from '../handlers/tracking.handler';
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const result = await TrackingHandler.createTracking(req.body);
+    res.json(result);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const result = await TrackingHandler.createTracking(req.body);
